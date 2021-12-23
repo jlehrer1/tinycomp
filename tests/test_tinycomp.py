8eaafe746cff519d82fe08a7efe9a2bcd3af30c1
Julian Lehrer <jmlehrer@ucsc.edu>
2021-12-22 19:40:05 -0800

    Initial commit

---
+++ tests/test_tinycomp.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `tinycomp` package."""
+
+
+import unittest
+
+from tinycomp import tinycomp
+
+
+class TestTinycomp(unittest.TestCase):
+    """Tests for `tinycomp` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
---
+++ tinycomp/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for BigSmall DataFrame."""
+
+__author__ = """Julian Lehrer"""
+__email__ = 'jmlehrer@ucsc.edu'
+__version__ = '0.1.0'
